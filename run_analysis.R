1bace506235a60d02a56dee7ec1e25e74407d71c
kimkennedy06 <kimberly.r.kennedy@gmail.com>
2014-04-20 16:50:03 -0700

    Creating Data Frames for Train and Test Data
    Read in data for both train and test data and created a data frame for
    each.

---
+++ run_analysis.R
@@ -0,0 +1,14 @@
+run_analysis <- function( ){
+
+  #Read in Test data and merge into singular Data Frame
+  x_test<-read.table("UCI HAR Dataset/test/X_test.txt")
+  y_test<-read.table("UCI HAR Dataset/test/y_test.txt")
+  subject_test<-read.table("UCI HAR Dataset/test/subject_test.txt")
+  TestDF<-cbind(x_test,y_test,subject_test)
+  
+  #Read in Train data and merge into singular Data Frame
+  x_train<-read.table("UCI HAR Dataset/train/X_train.txt")
+  y_train<-read.table("UCI HAR Dataset/train/y_train.txt")
+  subject_train<-read.table("UCI HAR Dataset/train/subject_train.txt")
+  TrainDF<-cbind(x_train,y_train,subject_train)
+}
