32e2b5360c4066993912a716e1b5df9fce350eac
kimkennedy06 <kimberly.r.kennedy@gmail.com>
2014-04-20 18:00:24 -0700

    Combined Test and Train into data frame
    Combined Test and Train data frames into a singular data frame and
    added labels based off feature list and general info

--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,21 @@
 run_analysis <- function( ){
-
+  #Read in Feature List
+  featuresTable<-read.table("UCI HAR Dataset/features.txt",col.names=c("id","feature"))
+  features<-as.character(featuresTable$feature)
+  
   #Read in Test data and merge into singular Data Frame
   x_test<-read.table("UCI HAR Dataset/test/X_test.txt")
   y_test<-read.table("UCI HAR Dataset/test/y_test.txt")
   subject_test<-read.table("UCI HAR Dataset/test/subject_test.txt")
-  TestDF<-cbind(x_test,y_test,subject_test)
+  TestDF<-cbind(subject_test,y_test,x_test)
   
   #Read in Train data and merge into singular Data Frame
   x_train<-read.table("UCI HAR Dataset/train/X_train.txt")
   y_train<-read.table("UCI HAR Dataset/train/y_train.txt")
   subject_train<-read.table("UCI HAR Dataset/train/subject_train.txt")
-  TrainDF<-cbind(x_train,y_train,subject_train)
+  TrainDF<-cbind(subject_train,y_train,x_train)
   
-  features<-read.table("UCI HAR Dataset/features.txt")
+  OverallData<-rbind(TestDF,TrainDF)
   
+  colnames(OverallData)<-c("Subject_ID","Activity_ID",features)
 }
