39a76ecf886f5cb6f21cef12219bc8a62bbb579c
kimkennedy06 <kimberly.r.kennedy@gmail.com>
2014-04-20 17:10:59 -0700

    Read in list of features
    Read in list of features for use as column names for overall data frame

--- run_analysis.R
+++ run_analysis.R
@@ -11,4 +11,7 @@ run_analysis <- function( ){
   y_train<-read.table("UCI HAR Dataset/train/y_train.txt")
   subject_train<-read.table("UCI HAR Dataset/train/subject_train.txt")
   TrainDF<-cbind(x_train,y_train,subject_train)
+  
+  features<-read.table("UCI HAR Dataset/features.txt")
+  
 }
